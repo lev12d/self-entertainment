16cbe382417c77283f9a347fe01fd7185a49767c
xhj <1411693091@qq.com>
2019-10-18 08:55:21 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
+import store from './store/index.js'
 Vue.config.productionTip = false
 
 Vue.use(VueAxios,Axios)
@@ -13,6 +14,7 @@ Vue.use(VueAxios,Axios)
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
